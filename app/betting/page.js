bcbe791cb5af203791c589b034a90624dde3c427
Filip Napiorkowski <filip.napiorkowski@xtremepush.com>
2023-12-10 17:10:37 +0100

    first commit

---
+++ app/betting/page.js
@@ -0,0 +1,9 @@
+const BettingPage = () => {
+    return(
+      <div>
+        <h1> Betting Page </h1>
+      </div>
+    );
+  };
+  
+  export default BettingPage;
---
+++ app/page.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return(
+    <div>
+      <h1 className="text-3xl font-bold"> Hello World</h1>
+    </div>
+  );
+};
+
+export default HomePage;
